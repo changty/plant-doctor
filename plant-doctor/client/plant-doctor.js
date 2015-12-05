e1d457632db628bb1193885911c43692eb11a160
Juhani <juhvaa@utu.fi>
2015-12-05 15:51:38 +0000

    New navigation

--- plant-doctor/client/plant-doctor.js
+++ plant-doctor/client/plant-doctor.js
@@ -1,4 +1,20 @@
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
+  Meteor.startup(function () {
+  });
 }
+
+Template.PlantDoctorTemplate.onRendered(function() {
+	// create navigation
+	var $nav = $('nav');
+	console.log('startup',$nav)
+	$nav.navigation({
+		gravity: 'push',
+		type: 'overlay',
+		maxWidth: '10000px'
+	});
+  	$('#main-nav > ul').find('a').click(function() {
+  		$nav.navigation('close');
+  	});
+});
--- plant-doctor/client/views/home.js
+++ plant-doctor/client/views/home.js
@@ -1,6 +1,6 @@
 Template.home.onRendered(function() {
+
 	Accounts.ui.config({
 		passwordSignupFields: "USERNAME_ONLY"
 	});
-	console.log("i'm ready!");
 });
