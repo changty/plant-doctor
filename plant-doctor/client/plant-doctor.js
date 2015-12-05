bd7b347be8982a196f4aa7b424b53b56c8c7e051
Juhani <juhvaa@utu.fi>
2015-12-05 22:01:41 +0000

    new speedometer

--- plant-doctor/client/plant-doctor.js
+++ plant-doctor/client/plant-doctor.js
@@ -5,15 +5,21 @@ if (Meteor.isClient) {
 
 Template.PlantDoctorTemplate.onRendered(function() {
 	// create navigation
-	var $nav = $('nav');
+	var $nav = $('#main-nav');
 	$nav.navigation({
 		gravity: 'push',
 		type: 'overlay',
 		maxWidth: '10000px'
 	});
-  	$('#main-nav > ul, header h1').find('a').click(function() {
-  		$nav.navigation('close');
-  	});
+	$('#main-nav > ul, header h1').find('a').click(function() {
+		$nav.navigation('close');
+		$nav.removeClass('fs-navigation-open');
+	});
+
+
+	$('body').on('click', '#btn-mobile-nav', function() {
+		$nav.toggleClass('fs-navigation-open');
+	});
 });
 
 Template.PlantDoctorTemplate.helpers({
--- plant-doctor/client/views/home.js
+++ plant-doctor/client/views/home.js
@@ -10,5 +10,21 @@ Template.home.onRendered(function() {
 Template.home.helpers({
 	userHasPlants: function() {
 		return MyPlant.find({owner: Meteor.userId()}).count() > 0 ? true : false;
+	},
+	moreThanOnePlant: function() {
+		return MyPlant.find({owner: Meteor.userId()}).count() > 1 ? true : false;
+	},
+	myPlants: function() {
+		return MyPlant.find({owner: Meteor.userId()});
+	},
+	getLuxDegrees: function() {
+		// replace with plant measurement value
+		var lux = 80;
+
+		// measure between 0 lux and 100 lux
+		var deg = 180 * (lux / 100);
+
+		// scale -90 -> 90
+		return deg - 90;
 	}
 });
